ffdbaa1eeee86d83dc4d051949f39bf38744ae61
Ishu Jain <102272183+IishuJainn@users.noreply.github.com>
2023-01-22 10:25:09 +0530

    Update Object_detection.py

--- Object_detection.py
+++ Object_detection.py
@@ -10,51 +10,61 @@ with open("coco.names","r") as f:
     classes=f.read().splitlines()
 print(classes)
 
-img=cv2.imread("test5.png")
-# img=cv2.resize(img,(224,224))
-height,width, _ =img.shape
-print(height,width)
-blob= cv2.dnn.blobFromImage(img,1/255, (320,320), (0,0,0),swapRB=True,crop=False)
-print(blob.shape)
-plt.imshow(img)
+import cv2
+cap=cv2.VideoCapture(0)
+while True:
+    ret, frame =cap.read()
+    img=frame
+    # img=cv2.imread("test4.png")
+    # img=cv2.resize(img,(224,224))
+    height,width, _ =img.shape
+    # print(height,width)
+    blob= cv2.dnn.blobFromImage(img,1/255, (320,320), (0,0,0),swapRB=True,crop=False)
+    print(blob.shape)
+    # plt.imshow(img)
 
-yolo.setInput(blob)
-output_layers_name=yolo.getUnconnectedOutLayersNames()
-layeroutput =yolo.forward(output_layers_name)
+    yolo.setInput(blob)
+    output_layers_name=yolo.getUnconnectedOutLayersNames()
+    layeroutput =yolo.forward(output_layers_name)
 
-boxs=[]
-confidences=[]
-class_ids=[]
-for output in layeroutput:
-    for detection in output:
-        score=detection[5:]
-        class_id=np.argmax(score)
-        confidence=score[class_id]
-        if confidence>0.7:
-            center_X=int(detection[0]*width)
-            center_y=int(detection[0]*height)
-            w=int(detection[0]*width)
-            h=int(detection[0]*height)
-            x=int(center_X-w/2)
-            y=int(center_y-h/2)
-            boxs.append([x,y,w,h])
-            confidences.append(float(confidence))
-            class_ids.append(class_id)
+    boxs=[]
+    confidences=[]
+    class_ids=[]
+    for output in layeroutput:
+        for detection in output:
+            score=detection[5:]
+            class_id=np.argmax(score)
+            confidence=score[class_id]
+            if confidence>0.7:
+                center_X=int(detection[0]*width)
+                center_y=int(detection[0]*height)
+                w=int(detection[0]*width)
+                h=int(detection[0]*height)
+                x=int(center_X-w/2)
+                y=int(center_y-h/2)
+                boxs.append([x,y,w,h])
+                confidences.append(float(confidence))
+                class_ids.append(class_id)
 
-indexs=cv2.dnn.NMSBoxes(boxs,confidences,.5,.4)
+    indexs=cv2.dnn.NMSBoxes(boxs,confidences,.5,.4)
 
-font=cv2.FONT_HERSHEY_PLAIN
-colors=np.random.uniform(0,255,size=(len(boxs),3))
+    font=cv2.FONT_HERSHEY_PLAIN
+    colors=np.random.uniform(0,255,size=(len(boxs),3))
 
-for i in indexs.flatten():
-    x,y,w,h =boxs[i]
-    label=str(classes[class_ids[i]])
-    confi=str(round(confidences[i],2))
-    color=colors[i]
-    print(boxs[i])
-    cv2.rectangle(img,(x,y),(x+w,y+h),color,3)
-    cv2.putText(img,label+" "+confi,(x,y+20),font,2,(255,0,0),2)
+    if len(indexs) > 0:
+        for i in indexs.flatten():
+            x,y,w,h =boxs[i]
+            label=str(classes[class_ids[i]])
+            confi=str(round(confidences[i],2))
+            color=colors[i]
+            print(boxs[i])
+            cv2.rectangle(img,(x,y),(x+w,y+h),color,3)
+            cv2.putText(img,label+" "+confi,(x,y+20),font,2,(255,0,0),2)
 
-plt.imshow(img)
-plt.show()
-# cv2.imwrite("./img.jpg",img)
+    cv2.imshow("window", img)
+    if cv2.waitKey(1) & 0xFF == ord('x'):
+        break
+cv2.destroyAllWindows()
+    # plt.imshow(img)
+    # plt.show()
+    # cv2.imwrite("./img.jpg",img)
